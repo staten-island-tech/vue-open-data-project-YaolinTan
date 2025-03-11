4114a86e4d967df1445e689d3579de3b7f4d1554
YaolinTan <yaolint@nycstudents.net>
2025-03-11 12:36:33 -0400

    e

--- app/src/components/UserSelection.vue
+++ app/src/components/UserSelection.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <div>
+  <div class="flex justify-center mt-[2rem]">
+    <div class="mr-[1rem]">
       <select v-model="selectedBorough" class="p-2 border">
-        <option>Select a Borough</option>
+        <option class="text-gray-400" value="" disabled selected>Select a Borough</option>
         <option
           class="text-black"
           v-for="borough in boroughs"
@@ -12,14 +12,24 @@
           {{ borough.label }}
         </option>
       </select>
-      <p>Selected Borough: {{ selectedBorough }}</p>
     </div>
-    <div>
-      <select>
-        <option>Pick a location type</option>
-        <option>
-          <label>Inside</label>
-          <label>Outside</label>
+    <div class="mr-[1rem]">
+      <select v-model="selectedInOrOut" class="p-2 border">
+        <option value="" disabled selected class="text-gray-400">Inside/Outside</option>
+        <option class="text-black" value="Inside">Inside</option>
+        <option class="text-black" value="Outside">Outside</option>
+      </select>
+    </div>
+    <div class="mr-[1rem]">
+      <select v-model="selectedCategory" class="p-2 border">
+        <option value="" disabled selected class="text-gray-400">Location Category</option>
+        <option
+          v-for="category in categories"
+          :key="category.value"
+          :value="category.value"
+          class="text-black"
+        >
+          {{ category.label }}
         </option>
       </select>
     </div>
@@ -30,12 +40,26 @@
 import { ref } from 'vue'
 
 const selectedBorough = ref('')
+const selectedInOrOut = ref('')
+const selectedCategory = ref('')
+
 const boroughs = [
-  { label: 'Bronx', value: 'bronx' },
-  { label: 'Brooklyn', value: 'brooklyn' },
-  { label: 'Manhattan', value: 'manhattan' },
-  { label: 'Queens', value: 'queens' },
-  { label: 'Staten Island', value: 'staten_island' },
+  { label: 'Bronx', value: 'Bronx' },
+  { label: 'Brooklyn', value: 'Brooklyn' },
+  { label: 'Manhattan', value: 'Manhattan' },
+  { label: 'Queens', value: 'Queens' },
+  { label: 'Staten Island', value: 'Staten Island' },
+]
+const categories = [
+  { label: 'Residential', value: 'residential' },
+  { label: 'Commercial', value: 'commercial' },
+  { label: 'Industrial', value: 'industrial' },
+]
+
+const location = [
+  { label: 'Apartment', value: 'apartment' },
+  { label: 'Townhouse', value: 'townhouse' },
+  { label: 'Condo', value: 'condo' },
 ]
 </script>
 
